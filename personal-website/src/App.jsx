b3013b5a411a077e49274a94dbaeb5574bcc1493
kevkimbu <kevkim@bu.edu>
2024-02-14 04:55:13 -0500

    Adjust path

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -3,10 +3,10 @@ import '@fontsource/roboto/100.css';
 
 import Navbar from './layouts/navbar'
 
-import Home from './pages/home'
-import About from './pages/about'
-import Projects from './pages/projects'
-import Contact from './pages/contact'
+import Home from './home/home'
+import About from './home/about'
+import Projects from './home/projects'
+import Contact from './home/contact'
 
 import './App.css'
 
