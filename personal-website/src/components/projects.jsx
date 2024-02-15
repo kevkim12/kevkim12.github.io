87ee6c1764b87abc7a6e0ae370ded56222c6fd1a
kevkimbu <kevkim@bu.edu>
2024-02-14 20:44:25 -0500

    Update project box

--- personal-website/src/components/projects.jsx
+++ personal-website/src/components/projects.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+import nightfall from '../assets/projects/Nightfall.png'
+
 const Container = styled.div`
   background-color: #000322;
   height: 100vh;
@@ -10,11 +12,42 @@ const Container = styled.div`
 `;
 
 const ProjectContainer = styled.div`
-  background-color: #fff;
-  width: 80%;
+  background-color: #a0a0a0;
 `;
 
 const Project = styled.div`
+  font-family: 'Roboto', sans-serif;
+  background-color: #101461;
+  width: 70%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  border-radius: 10px;
+  margin-left: auto;
+  margin-right: auto;
+  
+  img {
+    max-width: 95%;
+    height: 100%;
+    border-radius: 10px;
+    margin: auto;
+  }
+
+  h3 {
+    text-align: left;
+    color: #fff;
+    font-size: calc(1.5vw);
+    font-weight: 800;
+    margin-left: 3%;
+  }
+
+  p {
+    color: #fff;
+    margin-left: 3%;
+  }
+`;
+
+const ProjectDescription = styled.div`
 
 `;
 
@@ -23,9 +56,20 @@ function Projects() {
     <>
       <Container id="projects">
         <ProjectContainer>
-          <p>hi
-
-          </p>
+          <Project>
+            <div>
+              <img src={nightfall} alt="Nightfall"/>
+            </div>
+            <ProjectDescription>
+              <h3>Nightfall</h3>
+              <p>
+                Nightfall is a shooter game operated on a round-based system where players must eliminate
+                entities designated as the Infected. Players must either survive or complete objectives by
+                killing the zombies or satisfying missions. However, there are many different zombie classes
+                which can drastically change gameplay
+              </p>
+            </ProjectDescription>
+          </Project>
         </ProjectContainer>
 
       </Container>
