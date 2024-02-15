042458bbf2da55cd2bde075b65761fba6db5af66
kevkimbu <kevkim@bu.edu>
2024-02-14 19:16:31 -0500

    Add background graphic to home section

--- personal-website/src/components/home.jsx
+++ personal-website/src/components/home.jsx
@@ -1,5 +1,13 @@
-import styled from 'styled-components'
-import picture from '../assets/pfp.jpeg'
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import picture from '../assets/pfp.jpeg';
+import background from '../assets/backgrounds/wave.png';
+
+const Background = styled.div`
+  background-image: url(${background});
+  background-size: cover;
+  height: 120vh;
+`;
 
 const Introduction = styled.div`
   display: flex;
@@ -9,7 +17,7 @@ const Introduction = styled.div`
   justify-content: center;
   height: 80vh;
   gap: 5vw;
-`
+`;
 
 const PictureContainer = styled.div`
   position: relative;
@@ -20,53 +28,69 @@ const PictureContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
 const Picture = styled.img`
   width: 100%;
   height: auto;
   max-width: 400px;
   border-radius: 50%;
-`
+`;
 
 const Information = styled.div`
   display: flex;
   flex-direction: column;
   width: 50%;
   max-width: 50vw;
-`
+`;
 
 const Header = styled.h1`
+  color: #fff;
   font-family: 'Roboto', sans-serif;
   font-weight: 100;
   font-size: calc(3vw);
-`
+  opacity: 0;
+  transition: opacity 3s ease;
+`;
 
 const Description = styled.p`
+  color: #fff;
   font-family: 'Roboto', sans-serif;
   font-weight: 100;
   font-size: calc(2vw);
-`
+  opacity: 0;
+  transition: opacity 6s ease;
+`;
 
 const Fill = styled.div`
   height: 12vh;
-`
+  background-color: #a9a9a9;
+`;
 
 function Home() {
+  useEffect(() => {
+    const header = document.getElementById('header');
+    header.style.opacity = '1';
+    const description = document.getElementById('description');
+    description.style.opacity = '1';
+  }, []);
+
   return (
     <>
-      <Introduction id="home">
-        <Information>
-          <Header>Hi, I'm Kevin.</Header>
-          <Description>I'm a software and game developer based in Atlanta.</Description>
-        </Information>
-        <PictureContainer>
-          <Picture src={picture} alt="Kevin Kim"></Picture>
-        </PictureContainer>
-      </Introduction>
-      <Fill/>
+      <Background>
+        <Introduction id="home">
+          <Information>
+            <Header id="header">Hi, I'm Kevin.</Header>
+            <Description id="description">I'm a software and game developer based in Atlanta.</Description>
+          </Information>
+          <PictureContainer>
+            <Picture src={picture} alt="Kevin Kim"></Picture>
+          </PictureContainer>
+        </Introduction>
+      </Background>
+      <Fill />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
